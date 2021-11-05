1e04f434051f240dd65fc18935be11106d28162c
Mechansky <alexmaratovich@mail.ru>
2021-11-05 22:10:34 +0300

    homework 7

--- homework/geekshop/geekshop/urls.py
+++ homework/geekshop/geekshop/urls.py
@@ -27,8 +27,10 @@ urlpatterns = [
     path('products/', include('mainapp.urls', namespace='products')),
     path('basket/', include('basketapp.urls', namespace='basket')),
     path('auth/', include('authapp.urls', namespace='auth')),
+    path('admin/', include('adminapp.urls', namespace='adminapp')),
 
-    path('admin/', admin.site.urls),
+    path('control/', admin.site.urls),
+
 ]
 
 
